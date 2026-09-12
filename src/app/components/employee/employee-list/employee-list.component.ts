0a5d75dc98d698dda515c0eebc8f090fd1ca391a
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-12 13:04:25 +0530

    fix: update employee list without page reload

--- src/app/components/employee/employee-list/employee-list.component.ts
+++ src/app/components/employee/employee-list/employee-list.component.ts
@@ -5,28 +5,23 @@ import { Employee } from '../../../model/employee';
 @Component({
   selector: 'app-employee-list',
   standalone: false,
-  
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent implements OnInit{
-emp:Employee[];
+export class EmployeeListComponent implements OnInit {
+  emp: Employee[] = [];
 
-  constructor(public cs:CommonService) { }
+  constructor(public cs: CommonService) { }
 
   ngOnInit(): void {
-    this.cs.getData().subscribe((data:Employee[])=>{
- 
-      this.emp=data;})}
-
-  // deleteData(id:number){
-  //   this.cs.deleteData(id).subscribe((data:Employee[])=>{
-  //     this.emp=data;})}
-
-      deleteData(id:any){
-        this.cs.deleteData(id).subscribe();
-        window.location.reload();
-      }
-
+    this.cs.getData().subscribe((data: Employee[]) => {
+      this.emp = data;
+    });
   }
 
+  deleteData(id: number): void {
+    this.cs.deleteData(id).subscribe(() => {
+      this.emp = this.emp.filter(e => e.id !== id);
+    });
+  }
+}
