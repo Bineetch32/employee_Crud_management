ef0d50a2086f7989e007b5ae69fe574d9a1ad2e8
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-12 13:04:19 +0530

    fix: simplify employee API service and ID handling

--- src/app/service/common.service.ts
+++ src/app/service/common.service.ts
@@ -1,54 +1,48 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
 import { Employee } from '../model/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
-  
-  url:string="http://localhost:3000/Employee";
-  private nextId: number=1;
-  constructor(private http:HttpClient) { }
 
-  e:Employee=
-  {
+  url: string = "http://localhost:3000/Employee";
 
+  constructor(private http: HttpClient) { }
+
+  e: Employee = {
     id: 0,
-      ename: '',
-      email: '',
-      mobile: '',
-      designation: '',
-      username: '',
-      password: '',
+    ename: '',
+    email: '',
+    mobile: '',
+    designation: '',
+    username: '',
+    password: ''
   };
 
-
-  postdata(e:Employee)
-  {
-    e.id = this.nextId++; 
-    console.log(e);
-    return this.http.post(this.url,e);
+  postdata(e: Employee) {
+    return this.http.post(this.url, e);
   }
 
-getData(){
-  return this.http.get(this.url);
-}
+  getData() {
+    return this.http.get(this.url);
+  }
 
-deleteData(id:number){
-  return this.http.delete(this.url+"/"+id);
-}
+  deleteData(id: number) {
+    return this.http.delete(this.url + "/" + id);
+  }
 
-getDataById(id:number){
+  getDataById(id: number) {
+    return this.http.get(this.url + "/" + id);
+  }
 
-  return this.http.get(this.url+"/"+id);}
-
-  updateData(e:Employee){
-
+  updateData(e: Employee) {
     if (!e.id) {
-      console.error('Cannot update: Employee ID is missing.');
-      
+      return throwError(() => new Error('Employee ID is required for update.'));
     }
-    return this.http.put(this.url+"/"+e.id,e);
+
+    return this.http.put(this.url + "/" + e.id, e);
   }
 }
