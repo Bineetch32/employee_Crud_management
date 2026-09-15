fe9316d2d62bfe84fddc2abe107d7969eef5b572
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-15 17:50:54 +0530

    Fix login to validate registered employee credentials

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,41 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonService } from '../../service/common.service';
+import { Employee } from '../../model/employee';
 import { faLock } from "@fortawesome/free-solid-svg-icons";
-
 
 @Component({
   selector: 'app-login',
   standalone: false,
-  
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   faLock = faLock;
-loginForm:FormGroup;
-userType: any;
+  loginForm: FormGroup;
+  userType: any;
 
-  constructor(private fb:FormBuilder, private r:Router) { }
+  constructor(private fb: FormBuilder, private r: Router, private s: CommonService) { }
 
   ngOnInit(): void {
-    this.loginForm=this.fb.group({
-      username:'',
-      password:''
+    this.loginForm = this.fb.group({
+      username: '',
+      password: ''
     });
   }
 
-  onLoginSubmit(){
+  onLoginSubmit() {
+    this.s.getData().subscribe((data: any) => {
+      const employees: Employee[] = data;
 
-   if(this.loginForm.value.username=='user' && this.loginForm.value.password=='user123'){
-    console.log('User Login Success');
-    alert('User Login Success');
-    this.r.navigateByUrl('login/header'); 
-  }
-  else{
-    console.log('User Login Failed');
-    alert('User Login Failed')
-  }
-}
+      const employee = employees.find(e =>
+        e.username === this.loginForm.value.username &&
+        e.password === this.loginForm.value.password
+      );
 
+      if (employee) {
+        console.log('Login Success');
+        alert('Login Success');
+        this.r.navigateByUrl('login/header');
+      } else {
+        console.log('Login Failed');
+        alert('Invalid username or password');
+      }
+    });
+  }
 }
